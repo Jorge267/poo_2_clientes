717393f626ae89cb72d0954589a37e76e3c37f1f
Jorge267 <jaguilarg14@miumg.edu.gt>
2024-08-27 22:25:28 -0600

    Funcionalidad de formularios sin constructor

---
+++ src/vista/main.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package vista;
+
+
+public class main {
+
+    public static void main(String[] args) {
+       frm_empleado frm = new frm_empleado();
+       frm.show();
+    }
+    
+}
