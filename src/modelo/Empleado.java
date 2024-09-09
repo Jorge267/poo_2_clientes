e41bf4cdf41a4004491c6d677be684b58d266a7d
Jorge267 <jaguilarg14@miumg.edu.gt>
2024-09-08 22:10:26 -0600

    Se añadio la funcionalidad de insertar en la tabla empleados haciendo uso del combobox

--- src/modelo/Empleado.java
+++ src/modelo/Empleado.java
@@ -3,45 +3,70 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package modelo;
+import java.awt.HeadlessException;
+import javax.swing.JOptionPane;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;   //MOstrar el error con el try catch
+import java.sql.ResultSet;      //Utilizado para realizar la consulta a la base de datos
+import javax.swing.table.DefaultTableModel;
+
 
 
 public class Empleado extends Persona{
     
-    private String codigo_empleado, puesto;
+    private String codigo;
+    private int id_puesto;
+    Conexion cn;
 
     public Empleado(){}
-    
-    public Empleado(String nombres, String apellidos, String direccion, String telefono, String fecha_nacimiento, String codigo_empleado, String puesto) {
+    public Empleado(String nombres, String apellidos, String direccion, String telefono, String fecha_nacimiento, String codigo_empleado, int id_puesto) {
         super(nombres, apellidos, direccion, telefono, fecha_nacimiento);
-        this.codigo_empleado = codigo_empleado;
-        this.puesto = puesto;
+        this.codigo = codigo_empleado;
+        this.id_puesto = id_puesto;
     }
     
-    public String getCodigo_empleado() {
-        return codigo_empleado;
+    public String getCodigo() {
+        return codigo;
     }
 
-    public void setCodigo_empleado(String codigo_empleado) {
-        this.codigo_empleado = codigo_empleado;
+    public void setCodigo(String codigo_empleado) {
+        this.codigo = codigo_empleado;
     }
 
-    public String getPuesto() {
-        return puesto;
+    public int getId_puesto() {
+        return id_puesto;
     }
 
-    public void setPuesto(String puesto) {
-        this.puesto = puesto;
+    public void setId_puesto(int id_puesto) { 
+        this.id_puesto = id_puesto;
     }
     
     @Override
     public void agregar(){
-        System.out.println("Nombres " + getNombres());
-        System.out.println("Apellidos " + getApellidos());
-        System.out.println("Direccion " + getDireccion());
-        System.out.println("Telefono " + getTelefono());
-        System.out.println("Fecha Nacimiento " + getFecha_nacimiento());
-        System.out.println("Codigo Empleado" + getCodigo_empleado());
-        System.out.println("Puesto" + getPuesto());
+
+        try{
+          PreparedStatement parametro;
+          String query = "INSERT INTO empleados (codigo, nombres, apellidos, direccion, telefono, fecha_nacimiento, id_puesto) VALUES (?,?,?,?,?,?,?);      ";
+          cn = new Conexion();
+          cn.abrir_conexion();
+          parametro = (PreparedStatement) cn.conexionBD.prepareStatement(query);
+          parametro.setString(1, this.getCodigo());
+          parametro.setString(2, this.getNombres());
+          parametro.setString(3, this.getApellidos());
+          parametro.setString(4, this.getDireccion());
+          parametro.setString(5, this.getTelefono());
+          parametro.setString(6, this.getFecha_nacimiento());
+          parametro.setInt(7, this.getId_puesto());
+          System.out.println(getId_puesto());
+          
+          int ejecutar = parametro.executeUpdate();
+          cn.cerrar_conexion();
+          JOptionPane.showMessageDialog(null, Integer.toString(ejecutar) + " Registro ingresado", "Agregar", JOptionPane.INFORMATION_MESSAGE);
+        }catch(HeadlessException | SQLException ex){
+            System.out.println("Error..." + ex.getMessage());
+        }
     }
+
+
 
 }
--- src/vista/main.java
+++ src/vista/main.java
@@ -1,4 +1,4 @@
-/*
+    /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
@@ -8,7 +8,7 @@ package vista;
 public class main {
 
     public static void main(String[] args) {
-       frm_cliente frm = new frm_cliente();
+       frm_empleado frm = new frm_empleado();
        frm.show();
     }
     
